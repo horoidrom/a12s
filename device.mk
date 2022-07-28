5c52e776c00c316efac0ead77acb771574e83d34
horoid <59495159+horoidrom@users.noreply.github.com>
2022-07-28 09:39:45 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,11 +16,6 @@
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
     
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
